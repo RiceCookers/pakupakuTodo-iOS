ce6d55360be43069e0062ab64932329890ff1e4a
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-09-16 02:43:09 +0900

    first commit

---
+++ pakupakuTodo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  pakupakuTodo
+//
+//  Created by kazuma on 2018/09/16.
+//  Copyright © 2018年 kazuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
